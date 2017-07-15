d7d015bb6ec7b3eeca4c30ab769b941c88c8b5a5
dacup72 <dacup72@gmail.com>
2017-07-15 15:27:23 -0600

    init folders

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: '3qKIblNB8oVKaeWrMjYXagfrx',
+  consumer_secret: 'FIET4L22dJQjtTduberqPhJL4qBmYg7VxiTo8TwmUjg4IBdFaQ',
+  access_token_key: '504990437-j135rT6XjX7cbq7IIQ7QQJEG8QLNeu9iJnC5a4sL',
+  access_token_secret: 'oo2zB5bnV07bm6QsMxAGCuLTvbSxyglzlahU32YJyixJO',
+}
